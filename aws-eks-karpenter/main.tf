b5cec32350efd7413adf0546061cae7e390e3952
Lin <yenhlin@88665a0e13fc.ant.amazon.com>
2022-05-02 22:34:01 +0800

    first commit.

---
+++ aws-eks-karpenter/main.tf
@@ -0,0 +1,181 @@
+provider "aws" {
+  region = "ap-northeast-1"
+}
+
+locals {
+  cluster_name = "karpenter-cluster"
+}
+
+data "aws_eks_cluster" "cluster" {
+  name = module.eks.cluster_id
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.eks.cluster_id
+}
+
+data "aws_iam_policy" "ssm_managed_instance" {
+  arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  exec {
+    api_version = "client.authentication.k8s.io/v1alpha1"
+    args        = ["eks", "get-token", "--cluster-name", local.cluster_name]
+    command     = "aws"
+  }
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = data.aws_eks_cluster.cluster.endpoint
+    token                  = data.aws_eks_cluster_auth.cluster.token
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  }
+}
+
+data "aws_availability_zones" "available" {
+}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "3.14.0"
+
+  name = "${local.cluster_name}-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = data.aws_availability_zones.available.names
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_nat_gateway     = true
+  single_nat_gateway     = true
+  one_nat_gateway_per_az = false
+
+  private_subnet_tags = {
+    "kubernetes.io/cluster/${local.cluster_name}" = "owned"
+  }
+}
+
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "<18"
+
+  cluster_version = "1.21"
+  cluster_name    = local.cluster_name
+  vpc_id          = module.vpc.vpc_id
+  subnets         = module.vpc.private_subnets
+  enable_irsa     = true
+
+  cluster_create_timeout = "30m"
+
+  # Only need one node to get Karpenter up and running
+  worker_groups = [
+    {
+      instance_type = "t3a.medium"
+      asg_max_size  = 1
+    }
+  ]
+
+  map_users = [
+    {
+      userarn  = var.auth_map_user_arn
+      username = var.auth_map_username
+      groups   = ["system:masters"]
+    },
+  ]
+
+}
+
+resource "aws_iam_role_policy_attachment" "karpenter_ssm_policy" {
+  role       = module.eks.worker_iam_role_name
+  policy_arn = data.aws_iam_policy.ssm_managed_instance.arn
+}
+
+resource "aws_iam_instance_profile" "karpenter" {
+  name = "KarpenterNodeInstanceProfile-${local.cluster_name}"
+  role = module.eks.worker_iam_role_name
+}
+
+module "iam_assumable_role_karpenter" {
+  source                        = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
+  version                       = "4.7.0"
+  create_role                   = true
+  role_name                     = "karpenter-controller-${local.cluster_name}"
+  provider_url                  = module.eks.cluster_oidc_issuer_url
+  oidc_fully_qualified_subjects = ["system:serviceaccount:karpenter:karpenter"]
+}
+
+resource "aws_iam_role_policy" "karpenter_controller" {
+  name = "karpenter-policy-${local.cluster_name}"
+  role = module.iam_assumable_role_karpenter.iam_role_name
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "ec2:CreateLaunchTemplate",
+          "ec2:CreateFleet",
+          "ec2:RunInstances",
+          "ec2:CreateTags",
+          "iam:PassRole",
+          "ec2:TerminateInstances",
+          "ec2:DescribeLaunchTemplates",
+          "ec2:DescribeInstances",
+          "ec2:DescribeSecurityGroups",
+          "ec2:DescribeSubnets",
+          "ec2:DescribeInstanceTypes",
+          "ec2:DescribeInstanceTypeOfferings",
+          "ec2:DescribeAvailabilityZones",
+          "ssm:GetParameter"
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+      },
+    ]
+  })
+}
+
+resource "helm_release" "karpenter" {
+  depends_on       = [module.eks.kubeconfig]
+  namespace        = "karpenter"
+  create_namespace = true
+
+  name       = "karpenter"
+  repository = "https://charts.karpenter.sh"
+  chart      = "karpenter"
+  version    = "v0.5.3"
+
+  set {
+    name  = "serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
+    value = module.iam_assumable_role_karpenter.iam_role_arn
+  }
+
+  set {
+    name  = "controller.clusterName"
+    value = local.cluster_name
+  }
+
+  set {
+    name  = "controller.clusterEndpoint"
+    value = module.eks.cluster_endpoint
+  }
+}
+
+# This module is used to create K8s Cluster Autoscaler
+# module "eks-cluster-autoscaler" {
+#   source  = "lablabs/eks-cluster-autoscaler/aws"
+#   version = "1.6.1"
+
+#   cluster_name                     = module.eks.cluster_id
+#   cluster_identity_oidc_issuer     = module.eks.oidc_provider
+#   cluster_identity_oidc_issuer_arn = module.eks.oidc_provider_arn
+# }
+
+
+
+
+
---
+++ aws-eks-karpenter/variables.tf
@@ -0,0 +1,9 @@
+variable "auth_map_user_arn" {
+  description = "This value is used for auth map user arn. The reference is here (https://docs.aws.amazon.com/eks/latest/userguide/add-user-role.html)"
+  type        = string
+}
+
+variable "auth_map_username" {
+  description = "This value is used for auth map user arn. The reference is here (https://docs.aws.amazon.com/eks/latest/userguide/add-user-role.html)"
+  type        = string
+}
